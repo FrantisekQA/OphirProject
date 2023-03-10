632bb4fce7d3ab671bcec470737cbc3b99d691af
FrantisekQA <frantisek@wedoqa.co>
2023-03-10 08:25:58 +0100

    Updated the DrawRectangle function

--- src/test/java/Ophir/OpacityTest.java
+++ src/test/java/Ophir/OpacityTest.java
@@ -1,11 +1,14 @@
 package Ophir;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class OpacityTest {
     WebDriver driver;
@@ -17,6 +20,34 @@ class OpacityTest {
         driver = new ChromeDriver();
         actions = new Actions(driver);
         driver.manage().window().maximize();
+    }
+
+    @Test
+    @DisplayName("Change the opacity")
+    void ChangeOpacity() throws InterruptedException
+    {
+        driver.get("https://wbo.ophir.dev/");
+
+        Thread.sleep(1000);
+
+        //Create a private board with a name
+        WebElement boardNameField = driver.findElement(By.id("board"));
+        boardNameField.sendKeys("Test Board 2");
+        WebElement goButton = driver.findElement(By.xpath("//*[@id=\"named-board-form\"]/input[2]"));
+        goButton.click();
+
+        //Wait for the board to load
+        Thread.sleep(1000);
+
+        //Find the opacity tool
+        WebElement opacity = driver.findElement(By.id("opacityIndicator"));
+        actions.moveToElement(opacity).perform();
+        System.out.println("The mouse has been hovered over the opacity tool");
+
+        //Find the slider
+        WebElement opacitySlider = driver.findElement(By.id("chooseOpacity"));
+        actions.dragAndDropBy(opacitySlider,-50,0).perform();
+        System.out.println("The opacity has been lowered");
     }
 
 }
